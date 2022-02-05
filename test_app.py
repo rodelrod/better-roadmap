2597d31d98d83bc633654a18254e13ba6268e092
Rodrigo Eliseu <rodelrod@hotmail.com>
2022-02-05 15:37:02 +0100

    tests: Add dependencies

---
+++ test_app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from datetime import datetime
+
+import app
+
+
+class TestFreeSlots:
+    class TestNextSlots:
+        class TestNextUxSlots:
+            def test_first_feature(self):
+                assert False
