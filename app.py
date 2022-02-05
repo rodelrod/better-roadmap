2a9ba666af61cd65a83499ba36aaedee2663b8f8
Rodrigo Eliseu <rodelrod@hotmail.com>
2022-02-05 15:20:49 +0100

    feat: FreeSlots class, assigns slots to features

--- app.py
+++ app.py
@@ -21,6 +21,48 @@ class FeatureSpan:
     phase: str
 
 
+class FreeSlots:
+    """Keep track of the latest sprints for each phase."""
+
+    MAX_GAP_BETWEEN_CONCEPTION_AND_DEV = 4
+    MIN_GAP_BETWEEN_CONCEPTION_AND_DEV = 2
+    MIN_GAP_BETWEEN_UX_AND_CONCEPTION = 1
+
+    def __init__(
+        self, ux_max_concurrency=1, conception_max_concurrency=2, dev_max_concurrency=2
+    ) -> None:
+        self._next_ux_slots = tuple([1] * ux_max_concurrency)
+        self._next_conception_slots = tuple([1] * conception_max_concurrency)
+        self._next_dev_slots = tuple([1] * dev_max_concurrency)
+
+    def next_ux_slot(self):
+        return min(self._next_ux_slots)
+
+    def next_conception_slot(self, ux_estimation):
+        """Take Conception as soon as possible but not too early."""
+        return max(
+            # not too early before Dev, so that it does not need to be redone
+            self.next_dev_slot() - self.MAX_GAP_BETWEEN_CONCEPTION_AND_DEV,
+            min(
+                # in the first available conception slot…
+                min(self._next_conception_slots),
+                # …as long as it's after UX is done
+                self.next_ux_slot()
+                + ux_estimation
+                + self.MIN_GAP_BETWEEN_UX_AND_CONCEPTION,
+            ),
+        )
+
+    def next_dev_slot(self):
+        """Start dev in the next slot available but only if gap with conception is respected"""
+        return max(
+            # in the first available slot…
+            min(self._next_dev_slots),
+            # …as long as the conception is done
+            min(self._next_conception_slots) + self.MIN_GAP_BETWEEN_CONCEPTION_AND_DEV,
+        )
+
+
 def main():
     df = pd.DataFrame(
         [
