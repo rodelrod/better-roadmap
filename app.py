a147c1ee9510aaa9f95ee1853119c168d0647ac7
Rodrigo Eliseu <rodelrod@hotmail.com>
2022-02-06 16:09:00 +0100

    refactor: inject all dependencies to FreeSlots
    - Inject dependencies max_concurrency, gaps, and initial_state
    - Use dependency injection to easily test more complex cases
    - Fix bugs found with the new tests

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ FEATURES_FILE = Path("data", "features.yml")
 PARAMETERS_FILE = Path("data", "parameters.yml")
 
 
+class ArgumentError(Exception):
+    pass
+
+
 @dataclass
 class FeatureSpan:
     """Scheduled span for a Feature to be developed, ready to be put in the Roadmap."""
@@ -21,46 +25,76 @@ class FeatureSpan:
     phase: str
 
 
+@dataclass
+class FreeSlotsMaxConcurrency:
+    ux: int = 1
+    conception: int = 2
+    dev: int = 2
+
+
+@dataclass
+class FreeSlotsGaps:
+    max_gap_between_conception_and_dev: int = 3
+    min_gap_between_conception_and_dev: int = 1
+    min_gap_between_ux_and_conception: int = 0
+
+
 class FreeSlots:
     """Keep track of the latest sprints for each phase."""
 
-    MAX_GAP_BETWEEN_CONCEPTION_AND_DEV = 4
-    MIN_GAP_BETWEEN_CONCEPTION_AND_DEV = 2
-    MIN_GAP_BETWEEN_UX_AND_CONCEPTION = 0
+    CONCEPTION_ESTIMATION = 1
 
     def __init__(
-        self, ux_max_concurrency=1, conception_max_concurrency=2, dev_max_concurrency=2
+        self,
+        max_concurrency: FreeSlotsMaxConcurrency = None,
+        gaps: FreeSlotsGaps = None,
+        initial_state: dict[str, tuple] = None,
     ) -> None:
-        self._next_ux_slots = tuple([1] * ux_max_concurrency)
-        self._next_conception_slots = tuple([1] * conception_max_concurrency)
-        self._next_dev_slots = tuple([1] * dev_max_concurrency)
+        self.max_concurrency = max_concurrency or FreeSlotsMaxConcurrency()
+        self.gaps = gaps or FreeSlotsGaps()
+        if not initial_state:
+            self._next_ux_slots = tuple([1] * self.max_concurrency.ux)
+            self._next_conception_slots = tuple([1] * self.max_concurrency.conception)
+            self._next_dev_slots = tuple([1] * self.max_concurrency.dev)
+        else:
+            self.set_state(initial_state)
 
-    def next_ux_slot(self):
+    def set_state(self, initial_state: dict[str, tuple]) -> None:
+        for phase in ["ux", "conception", "dev"]:
+            if len(initial_state[phase]) != getattr(self.max_concurrency, phase):
+                raise ArgumentError("Initial_state is malformed")
+            else:
+                setattr(self, f"_next_{phase}_slots", initial_state[phase])
+
+    def next_ux_slot(self) -> int:
         return min(self._next_ux_slots)
 
-    def next_conception_slot(self, ux_estimation):
+    def next_conception_slot(self, ux_estimation) -> int:
         """Take Conception as soon as possible but not too early."""
         return max(
             # not too early before Dev, so that it does not need to be redone
-            min(self._next_dev_slots) - self.MAX_GAP_BETWEEN_CONCEPTION_AND_DEV,
+            min(self._next_dev_slots)
+            - self.gaps.max_gap_between_conception_and_dev
+            - self.CONCEPTION_ESTIMATION,
             max(
                 # in the first available conception slot…
                 min(self._next_conception_slots),
                 # …as long as it's after UX is done
                 self.next_ux_slot()
                 + ux_estimation
-                + self.MIN_GAP_BETWEEN_UX_AND_CONCEPTION,
+                + self.gaps.min_gap_between_ux_and_conception,
             ),
         )
 
-    def next_dev_slot(self, ux_estimation):
+    def next_dev_slot(self, ux_estimation) -> int:
         """Start dev in the next slot available but only if gap with conception is respected"""
         return max(
             # in the first available slot…
             min(self._next_dev_slots),
             # …as long as the conception is done
             self.next_conception_slot(ux_estimation)
-            + self.MIN_GAP_BETWEEN_CONCEPTION_AND_DEV,
+            + self.CONCEPTION_ESTIMATION
+            + self.gaps.min_gap_between_conception_and_dev,
         )
 
 
--- test_app.py
+++ test_app.py
@@ -30,3 +30,53 @@ class TestFreeSlots:
 
                 def test_dev_is_slotted_on_fourth_slot(self, free_slots_empty):
                     assert free_slots_empty.next_dev_slot(ux_estimation=3) == 6
+
+        class TestWhenDevTeamIsLate:
+            @pytest.fixture
+            def free_slots_late_dev(self):
+                initial_state = {"ux": (3,), "conception": (3, 3), "dev": (10, 11)}
+                return sut.FreeSlots(initial_state=initial_state)
+
+            def test_dev_is_slotted_on_first_available_slot_ie_ten(
+                self, free_slots_late_dev
+            ):
+                assert free_slots_late_dev.next_dev_slot(ux_estimation=1) == 10
+
+            def test_conception_is_slotted_three_sprints_before_dev_and_not_before(
+                self, free_slots_late_dev
+            ):
+                assert free_slots_late_dev.next_conception_slot(ux_estimation=1) == 6
+
+    class TestSetState:
+        class TestWhenInitialStateIsMalformed:
+            def test_when_too_many_slots_in_ux_raise(self):
+                max_concurrency = sut.FreeSlotsMaxConcurrency(ux=1, conception=1, dev=1)
+                with pytest.raises(sut.ArgumentError):
+                    sut.FreeSlots(
+                        max_concurrency=max_concurrency,
+                        initial_state={"ux": (1, 1), "conception": (1,), "dev": (1,)},
+                    )
+
+            def test_when_too_few_slots_in_dev_raise(self):
+                max_concurrency = sut.FreeSlotsMaxConcurrency(ux=1, conception=1, dev=4)
+                with pytest.raises(sut.ArgumentError):
+                    sut.FreeSlots(
+                        max_concurrency=max_concurrency,
+                        initial_state={
+                            "ux": (1,),
+                            "conception": (1,),
+                            "dev": (
+                                1,
+                                1,
+                                1,
+                            ),
+                        },
+                    )
+
+        class TestWhenInitialStateIsWellformed:
+            def test_when_slots_are_correct_do_not_raise(self):
+                max_concurrency = sut.FreeSlotsMaxConcurrency(ux=2, conception=1, dev=3)
+                sut.FreeSlots(
+                    max_concurrency=max_concurrency,
+                    initial_state={"ux": (1, 1), "conception": (1,), "dev": (1, 1, 1)},
+                )
