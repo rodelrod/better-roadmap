18a41ebe62924acffd406be42377586276f08550
Rodrigo Eliseu <rodelrod@hotmail.com>
2022-02-07 09:45:14 +0100

    feat: Convert sprint numbers to dates

--- span.py
+++ span.py
@@ -29,9 +29,36 @@ class ScheduledDateSpan:
     start: datetime
     end: datetime
 
+    @classmethod
+    def from_scheduled_sprint_span(
+        cls, sprint_span: ScheduledSprintSpan, project_start: datetime
+    ):
+        return cls(
+            start=sprint_to_start_date(
+                sprint=sprint_span.start, project_start=project_start
+            ),
+            end=sprint_to_end_date(sprint=sprint_span.end, project_start=project_start),
+        )
+
 
 @dataclass
 class ScheduledDateSpans:
     ux: ScheduledDateSpan
     conception: ScheduledDateSpan
     dev: ScheduledDateSpan
+
+    @classmethod
+    def from_scheduled_sprint_spans(
+        cls, sprint_spans: ScheduledSprintSpans, project_start: datetime
+    ):
+        return cls(
+            ux=ScheduledDateSpan.from_scheduled_sprint_span(
+                sprint_spans.ux, project_start
+            ),
+            conception=ScheduledDateSpan.from_scheduled_sprint_span(
+                sprint_spans.conception, project_start
+            ),
+            dev=ScheduledDateSpan.from_scheduled_sprint_span(
+                sprint_spans.dev, project_start
+            ),
+        )
--- test_span.py
+++ test_span.py
@@ -13,3 +13,25 @@ class TestSprintToDate:
         assert sut.sprint_to_end_date(
             sprint=3, project_start=datetime(2022, 1, 6)
         ) == datetime(2022, 1, 27)
+
+
+class TestScheduledSpan:
+    def test_sprint_to_date_spans(self):
+        sss = sut.ScheduledSprintSpans(
+            ux=sut.ScheduledSprintSpan(1, 3),
+            conception=sut.ScheduledSprintSpan(4, 4),
+            dev=sut.ScheduledSprintSpan(6, 8),
+        )
+        sts = sut.ScheduledDateSpans(
+            ux=sut.ScheduledDateSpan(datetime(2022, 1, 6), datetime(2022, 1, 27)),
+            conception=sut.ScheduledDateSpan(
+                datetime(2022, 1, 27), datetime(2022, 2, 3)
+            ),
+            dev=sut.ScheduledDateSpan(datetime(2022, 2, 10), datetime(2022, 3, 3)),
+        )
+        assert (
+            sut.ScheduledDateSpans.from_scheduled_sprint_spans(
+                sss, datetime(2022, 1, 6)
+            )
+            == sts
+        )
