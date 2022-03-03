ef113b3026d9f43b9069a59d708a0b6289cd8d8f
Rodrigo Eliseu <rodelrod@hotmail.com>
2022-03-03 15:57:46 +0100

    fix: Enforce order of features and phases
    Order is determined by order in file instead of time criteria.
    Previously we were seeing the features with no UX phase show up at the
    end of the roadmap.

--- better_roadmap/services/roadmap_chart.py
+++ better_roadmap/services/roadmap_chart.py
@@ -25,13 +25,31 @@ class RoadmapChart:
             Optional[tuple[str, str, str]],
             (elapsed_text, features_text, parameters_text),
         )
+        parameters = Parameters.from_text(parameters_text)
+        elapsed_feature_list = ElapsedFeatureList.from_text(elapsed_text)
+        feature_list = FeatureList.from_text(features_text)
+        ordered_phase_names = [phase.name for phase in parameters.phases]
+        ordered_elapsed_features = [
+            elapsed_feature.name for elapsed_feature in elapsed_feature_list
+        ]
+        ordered_features = [feature.name for feature in feature_list]
         scheduled_sprints = pd.DataFrame(
-            self._get_graph_segments(elapsed_text, features_text, parameters_text)
+            self._get_graph_segments(parameters, elapsed_feature_list, feature_list)
         )
-        self.figure = self._configure_chart(scheduled_sprints)
+        self.figure = self._configure_chart(
+            scheduled_sprints,
+            ordered_phase_names,
+            ordered_elapsed_features,
+            ordered_features,
+        )
 
     @staticmethod
-    def _configure_chart(df: pd.DataFrame) -> Figure:
+    def _configure_chart(
+        df: pd.DataFrame,
+        ordered_phase_names,
+        ordered_elapsed_features,
+        ordered_features,
+    ) -> Figure:
         fig = px.timeline(
             df,
             x_start="start",
@@ -40,21 +58,22 @@ class RoadmapChart:
             color="phase",
             height=720,
             color_discrete_sequence=px.colors.qualitative.Pastel,
+            # This is necessary to ensure that the order of features in the YAML is respected
+            category_orders={
+                "phase": ordered_phase_names,
+                "feature": ordered_elapsed_features + ordered_features,
+            },
         )
-        fig.update_yaxes(autorange="reversed")
         fig.add_vline(x=date.today())
         return fig
 
     @staticmethod
     def _get_graph_segments(
-        elapsed_text: Optional[str],
-        features_text: Optional[str],
-        parameters_text: Optional[str],
+        parameters, elapsed_feature_list, feature_list
     ) -> list[GraphSegment]:
         graph_segments = []
-        parameters = Parameters.from_text(parameters_text)
         scheduler = FeatureScheduler(parameters.phases)
-        for elapsed_feature in ElapsedFeatureList.from_text(elapsed_text):
+        for elapsed_feature in elapsed_feature_list:
             elapsed_feature_segments = scheduler.schedule_feature_as_dates(
                 elapsed_feature,
                 parameters.project_start,
@@ -62,7 +81,7 @@ class RoadmapChart:
                 parameters.sprint_durations,
             )
             graph_segments.extend(elapsed_feature_segments)
-        for feature in FeatureList.from_text(features_text):
+        for feature in feature_list:
             feature_segments = scheduler.schedule_feature_as_dates(
                 feature,
                 parameters.project_start,
